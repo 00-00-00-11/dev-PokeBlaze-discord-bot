b9feaf713c6c6250059895f2f59c955fb2fc1129
RoMeAh <62307220+RoMeAh@users.noreply.github.com>
2020-06-01 15:14:35 +0530

    YaY. Random Poke Generator....

--- Src/Lib/WebScrap/webscrap.js
+++ Src/Lib/WebScrap/webscrap.js
@@ -5,17 +5,38 @@ async function scrapeProduct ( url ) {
     const page = await browser.newPage ();
     await page.goto ( url );
 
-    const [ el ] = await page.$x ( '/html/body/main/div[3]/div[1]/span[1]/a/img' );
-    const src = await el.getProperty ( 'src' );
-    const pokepic1 = await src.jsonValue ();
+    var pokenum1 = (Math.floor ( Math.random () * 151 ));
+console.log(pokenum1)
+    if (pokenum1.toString ().length === 3) {
+        const pokepic1 = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${ pokenum1 }.png`
 
-    const [ el2 ] = await page.$x ( '/html/body/main/div[3]/div[1]/span[2]/a' );
-    const txt = await el2.getProperty ( 'textContent' );
-    const pokename1 = await txt.jsonValue ();
+        const [ el2 ] = await page.$x ( `html/body/main/div[3]/div[${ pokenum1 }]/span[2]/a` );
+        const txt = await el2.getProperty ( 'textContent' );
+        const pokename1 = await txt.jsonValue ();
 
-    console.log ( { pokepic1,pokename1 } )
+        console.log ( { pokepic1,pokename1 } )
+    } else
+    if (pokenum1.toString ().length === 2) {
+        const pokepic1 = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/"+"0"+`${ pokenum1 }.png`
+
+        const [ el2 ] = await page.$x ( `html/body/main/div[3]/div[${ pokenum1 }]/span[2]/a` );
+        const txt = await el2.getProperty ( 'textContent' );
+        const pokename1 = await txt.jsonValue ();
+
+        console.log ( { pokepic1, pokename1 } )
+    } else
+        if (pokenum1.toString ().length === 1) {
+            const pokepic1 = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/"+"00"+`${ pokenum1 }.png`
+
+            const [ el2 ] = await page.$x ( `html/body/main/div[3]/div[${ pokenum1 }]/span[2]/a` );
+            const txt = await el2.getProperty ( 'textContent' );
+            const pokename1 = await txt.jsonValue ();
+
+            console.log ( { pokepic1, pokename1 } )
+        }
+
 
     browser.close ();
 }
 
-scrapeProduct ( 'https://pokemondb.net/pokedex/national' );
+scrapeProduct ( 'https://pokemondb.net/pokedex/national/' );
