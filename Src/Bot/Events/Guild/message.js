76c9faa0b53b985f2d59842920f6c07ab6dc39b9
King Kheav <williamkheav@hotmail.com>
2020-06-01 20:54:44 +1000

    random pokemon

--- Src/Bot/Events/Guild/message.js
+++ Src/Bot/Events/Guild/message.js
@@ -1,9 +1,53 @@
+const { MessageEmbed } = require("discord.js");
+const gay = new Set();
+
 class Message {
     constructor ( client ) {
         this.client = client;
+        this.set = gay;
     }
 
     async run ( message ) {
+
+        if (message.author.bot) return;
+
+        const chance = Math.floor(Math.random() * 100 + 1);
+
+        if (chance >= 50) {
+
+            if (!this.set.has(message.guild.id)) {
+
+                const gay = require("../../../Lib/WebScrap/webscrap");
+
+                const retard = await gay("https://pokemondb.net/pokedex/national/");
+                console.log(retard);
+    
+                const embed = new MessageEmbed()
+                .setTitle("A pokemon has spawned")
+                .setImage(retard.pokepic1);
+    
+                await message.channel.send(embed);
+    
+                const filter = m => !m.bot && m.author.id != this.client.user.id;
+                const collector = message.channel.createMessageCollector(filter);
+    
+                collector.on("collect", async m => {
+    
+                    if (m.content.toLowerCase() === retard.pokename1.toLowerCase()) {
+                        message.channel.send("woah you're not a retard and guessed correctly");
+                        await collector.stop();
+                    } else {
+                        message.channel.send("retard you guessed incorrectly");
+                    };
+                });
+    
+                await this.set.add(message.guild.id);
+
+                setTimeout(() => this.set.delete(message.guild.id), 30000);
+            };
+
+        };
+
         if ( message.author.bot || !message.content.startsWith ( "b!" ) ) return;
         const args = message.content.split ( /\s+/g );
         const command = args.shift ().slice ( "b!".length );
@@ -13,6 +57,7 @@ class Message {
         cmd.setMessage ( message );
         cmd.run ( this.client , message , args );
         if ( cmd.conf.cooldown > 0 ) cmd.startCooldown ( message.author.id );
+
     }
 }
 
